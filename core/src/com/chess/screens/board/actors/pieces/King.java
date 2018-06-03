05ebd41c8fb1fc638d9906e3d38623025fca9880
Javier <takatsho@gmail.com>
2018-06-03 13:31:56 +0200

    Implemented king movement behaviour.

--- core/src/com/chess/screens/board/actors/pieces/King.java
+++ core/src/com/chess/screens/board/actors/pieces/King.java
@@ -10,7 +10,7 @@ import com.chess.screens.board.StateMachine;
 public class King extends Piece {
 
     public King(Chess.PLAYER player, Texture texture, int i, int j, StateMachine stateMachine) {
-        super(texture, player,stateMachine);
+        super(texture, player, stateMachine);
         setBoardPosition(i, j);
         addListener(new InputListener() {
             @Override
@@ -27,5 +27,62 @@ public class King extends Piece {
         float bottomLeftX = xCenterPosition - width / 2;
         float bottomLeftY = yCenterPosition - height / 2;
         batch.draw(texture, bottomLeftX, bottomLeftY, width, height);
+    }
+
+    @Override
+    public int[][] createBehaviourMap(int[][] collisionMap) {
+        int x = getxBoardCoord();
+        int y = getyBoardCoord();
+        int[][] behaviourMap = collisionMap.clone();
+
+        if (x + 1 < 8 && y + 1 < 8) {
+            if (collisionMap[x + 1][y + 1] == 0) {
+                behaviourMap[x + 1][y + 1] = 2;
+            }
+        }
+
+        if (x + 1 < 8 && y < 8) {
+            if (collisionMap[x + 1][y] == 0) {
+                behaviourMap[x + 1][y] = 2;
+            }
+        }
+
+
+        if (x + 1 < 8 && y - 1 >= 0) {
+            if (collisionMap[x + 1][y - 1] == 0) {
+                behaviourMap[x + 1][y - 1] = 2;
+            }
+        }
+
+        if (x < 8 && y - 1 >= 0) {
+            if (collisionMap[x][y - 1] == 0) {
+                behaviourMap[x][y - 1] = 2;
+            }
+        }
+
+        if (x - 1 >= 0 && y - 1 >= 0) {
+            if (collisionMap[x - 1][y - 1] == 0) {
+                behaviourMap[x - 1][y - 1] = 2;
+            }
+        }
+
+        if (x - 1 >= 0 && y >= 0) {
+            if (collisionMap[x - 1][y] == 0) {
+                behaviourMap[x - 1][y] = 2;
+            }
+        }
+
+        if (x - 1 >= 0 && y + 1 < 8) {
+            if (collisionMap[x - 1][y + 1] == 0) {
+                behaviourMap[x - 1][y + 1] = 2;
+            }
+        }
+
+        if (x < 8 && y + 1 < 8) {
+            if (collisionMap[x][y + 1] == 0) {
+                behaviourMap[x][y + 1] = 2;
+            }
+        }
+        return behaviourMap;
     }
 }

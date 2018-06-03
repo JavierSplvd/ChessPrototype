2837cbd7c6825bfcc26873fd819093d4d4e3ee42
Javier <takatsho@gmail.com>
2018-06-03 13:17:41 +0200

    Implemented bishop movement behaviour.

--- core/src/com/chess/screens/board/actors/pieces/Bishop.java
+++ core/src/com/chess/screens/board/actors/pieces/Bishop.java
@@ -28,4 +28,62 @@ public class Bishop extends Piece {
         float bottomLeftY = yCenterPosition - height / 2;
         batch.draw(texture, bottomLeftX, bottomLeftY, width, height);
     }
+
+    @Override
+    public int[][] createBehaviourMap(int[][] collisionMap) {
+        int x = getxBoardCoord();
+        int y = getyBoardCoord();
+        int[][] behaviourMap = collisionMap.clone();
+
+        //  Diagonal NE
+        int i = x + 1;
+        int j = y + 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i++;
+            j++;
+        }
+        // Diagonal SE
+        i = x + 1;
+        j = y - 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i++;
+            j--;
+        }
+        // Diagonal NW
+        i = x - 1;
+        j = y + 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i--;
+            j++;
+        }
+        // Diagonal SW
+        i = x - 1;
+        j = y - 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i--;
+            j--;
+        }
+
+        return behaviourMap;
+    }
 }

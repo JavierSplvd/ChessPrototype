66ad21a4c8cdcf3d0149746ef9e9d999752c7159
Javier <takatsho@gmail.com>
2018-06-03 13:38:11 +0200

    Implemented queen and rook movement behaviour.

--- core/src/com/chess/screens/board/actors/pieces/Queen.java
+++ core/src/com/chess/screens/board/actors/pieces/Queen.java
@@ -10,7 +10,7 @@ import com.chess.screens.board.StateMachine;
 public class Queen extends Piece {
 
     public Queen(Chess.PLAYER player, Texture texture, int i, int j, StateMachine stateMachine) {
-        super(texture, player,stateMachine);
+        super(texture, player, stateMachine);
         setBoardPosition(i, j);
         addListener(new InputListener() {
             @Override
@@ -27,5 +27,107 @@ public class Queen extends Piece {
         float bottomLeftX = xCenterPosition - width / 2;
         float bottomLeftY = yCenterPosition - height / 2;
         batch.draw(texture, bottomLeftX, bottomLeftY, width, height);
+    }
+
+    @Override
+    public int[][] createBehaviourMap(int[][] collisionMap) {
+        int x = getxBoardCoord();
+        int y = getyBoardCoord();
+        int[][] behaviourMap = collisionMap.clone();
+
+        //  Diagonal NE
+        int i = x + 1;
+        int j = y + 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i++;
+            j++;
+        }
+        // Diagonal SE
+        i = x + 1;
+        j = y - 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i++;
+            j--;
+        }
+        // Diagonal NW
+        i = x - 1;
+        j = y + 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i--;
+            j++;
+        }
+        // Diagonal SW
+        i = x - 1;
+        j = y - 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i--;
+            j--;
+        }
+        // Direction N
+        i = x;
+        j = y + 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            j++;
+        }
+        // Direction S
+        i = x;
+        j = y - 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            j--;
+        }
+        // Direction E
+        i = x + 1;
+        j = y;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i++;
+        }
+        // Direction W
+        i = x - 1;
+        j = y;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i--;
+        }
+
+        return behaviourMap;
     }
 }
--- core/src/com/chess/screens/board/actors/pieces/Rook.java
+++ core/src/com/chess/screens/board/actors/pieces/Rook.java
@@ -28,4 +28,60 @@ public class Rook extends Piece {
         float bottomLeftY = yCenterPosition - height / 2;
         batch.draw(texture, bottomLeftX, bottomLeftY, width, height);
     }
+
+    @Override
+    public int[][] createBehaviourMap(int[][] collisionMap) {
+        int x = getxBoardCoord();
+        int y = getyBoardCoord();
+        int[][] behaviourMap = collisionMap.clone();
+
+        int i;
+        int j;
+        // Direction N
+        i = x;
+        j = y + 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            j++;
+        }
+        // Direction S
+        i = x;
+        j = y - 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            j--;
+        }
+        // Direction E
+        i = x + 1;
+        j = y;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i++;
+        }
+        // Direction W
+        i = x - 1;
+        j = y;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (collisionMap[i][j] == 0) {
+                behaviourMap[i][j] = 2;
+            } else {
+                break;
+            }
+            i--;
+        }
+
+        return behaviourMap;
+    }
 }
